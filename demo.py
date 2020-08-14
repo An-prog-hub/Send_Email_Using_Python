20b346679da736cf33f296c52b889d5239990169
Ankush Chauhan <67872399+An-prog-hub@users.noreply.github.com>
2020-08-14 15:02:51 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+from_addr = 'ac.ankushchauhan.2006@gmail.com'
+to_addr = ['cutepie.anjali@gmail.com','ac.chauhan1102@gmail.com']
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To']=" ,".join(to_addr)
+msg['subject'] = 'just to check'
+
+body = 'Hello What is going on !!!!!!!!!!'
+
+msg.attach(MIMEText(body,'plain'))
+
+email = "ac.ankushchauhan.2006@gmail.com"
+password = "ankush@592"
+
+mail=smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login(email,password)
+text = msg.as_string()
+mail.sendmail(from_addr,to_addr,text)
+mail.quit()
